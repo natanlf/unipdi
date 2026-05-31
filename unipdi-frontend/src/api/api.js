dcd9d1bccfe11acbe005d264db182a1252532997
Natan <natanlaraferreira@gmail.com>
2026-05-31 14:53:20 -0300

    Aula 4 - Multi stage build com DockerFile e teste docker compose

--- unipdi-frontend/src/api/api.js
+++ unipdi-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080", // backend Spring Boot
+  baseURL: import.meta.env.VITE_API_URL || "/api", // backend Spring Boot
 });
 
 export default api;
